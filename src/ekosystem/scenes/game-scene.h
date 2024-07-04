acc0c665e3fe79d2ba4cd5e452a21ceed4df01be
Guthen <114919245+arkaht@users.noreply.github.com>
2024-07-04 23:38:20 +0200

    [init] project

---
+++ src/ekosystem/scenes/game-scene.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <suprengine/scene.h>
+#include <suprengine/components/colliders/box-collider.h>
+
+#include <ekosystem/entities/pawn.h>
+#include <ekosystem/components/camera-controller.h>
+#include <ekosystem/debug-menu.h>
+
+using namespace suprengine;
+
+namespace eks
+{
+	class GameScene : public Scene
+	{
+	public:
+		GameScene();
+		virtual ~GameScene();
+
+		void init() override;
+
+		void setup_world();
+
+		void update( float dt ) override;
+
+	private:
+		World* _world { nullptr };
+		SafePtr<CameraController> _camera_controller;
+		DebugMenu _debug_menu;
+	};
+}
